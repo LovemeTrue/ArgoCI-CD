fefacbb142fa5babc0fdbe6bc8eeafd71701945c
Dmitrii <legkodogatca@gmail.com>
2025-07-21 21:54:14 +0300

    debug false

--- ssl/ssl-gen.sh
+++ ssl/ssl-gen.sh
@@ -45,7 +45,9 @@ subjectAltName = @alt_names
 
 [ alt_names ]
 DNS.1 = ${DOMAIN}
-IP.1 = 192.168.29.24
+IP.1 = 92.217.196.189
+IP.2 = 92.217.196.183
+IP.3 = 92.217.196.156
 
 EOF
 
@@ -64,8 +66,8 @@ subjectAltName = @alt_names
 
 [alt_names]
 DNS.1 = ${DOMAIN}
-DNS.2 = *.elewise.local
-DNS.3 = master.elewise.local
+DNS.2 = *.sale.elewise.com
+DNS.3 = master.sale.elewise.com
 
 EOF
 
@@ -96,27 +98,18 @@ chmod -R 777 ${DOMAIN}.key
 chmod -R 777 rootCA.key
 chmod -R 777 rootCA_unencrypted.key
 
+# cp to /etc/ssl
+cp ${DOMAIN}.crt /etc/ssl/certs/
+cp ${DOMAIN}.key /etc/ssl/private/
+cp rootCA.crt  /etc/ssl/certs/
+
 echo "Certificate created for ${DOMAIN}"
 
-kubectl delete ns elma365
-kubectl delete ns elma365-dbs
-kubectl create ns elma365
-kubectl create ns elma365-dbs
+echo "Certificate created for ${DOMAIN}"
 
 
-kubectl label ns elma365 security.deckhouse.io/pod-policy=privileged --overwrite
-
-kubectl patch nodegroup master --type=merge -p '{"spec":{"kubelet":{"maxPods":200}}}'
-
-kubectl create secret tls elma365-onpremise-tls --cert=/home/kind/ssl/${DOMAIN}.crt --key=/home/kind/ssl/${DOMAIN}.key -n elma365-dbs
-kubectl create secret tls elma365-onpremise-tls --cert=/home/kind/ssl/${DOMAIN}.crt --key=/home/kind/ssl/${DOMAIN}.key -n elma365
-kubectl create configmap elma365-onpremise-ca --from-file=elma365-onpremise-ca.pem=/home/kind/ssl/rootCA.pem -n elma365
+kubectl create secret tls elma365-onpremise-tls --cert=/home/vrstel/ssl/${DOMAIN}.crt --key=/home/vrstel/ssl/${DOMAIN}.key -n elma365-dbs
+kubectl create secret tls elma365-onpremise-tls --cert=/home/vrstel/ssl/${DOMAIN}.crt --key=/home/vrstel/ssl/${DOMAIN}.key -n elma365
+kubectl create configmap elma365-onpremise-ca --from-file=elma365-onpremise-ca.pem=/home/vrstel/ssl/rootCA.pem -n elma365
 
 echo "Secret elma365-onpremise-tls and configmap elma365-onpremise-ca has been created for elma365, elma365-dbs. Please upgrade your helm charts"
-
-
-# kubectl create secret tls elma365-onpremise-tls --cert=/home/kind/ssl/${DOMAIN}.crt --key=/home/kind/ssl/${DOMAIN}.key -n elma365-dbs
-# kubectl create secret tls elma365-onpremise-tls --cert=/home/kind/ssl/${DOMAIN}.crt --key=/home/kind/ssl/${DOMAIN}.key -n elma365
-# kubectl create secret tls elma365-onpremise-ca --cert=/home/kind/ssl/rootCA.crt --key=/home/kind/ssl/rootCA_unencrypted.key -n elma365
-# kubectl create configmap elma365-onpremise-ca --from-file=elma365-onpremise-ca.pem=/home/kind/ssl/rootCA.pem -n elma365
-#  echo "Secret elma365-onpremise-tls and configmap elma365-onpremise-ca has been created for elma365, elma365-dbs. Please upgrade your helm charts"
